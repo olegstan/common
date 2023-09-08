c90edcf580db9c3cf6b9ac37c27c79871709c093
olegstan <olegstan@inbox.ru>
2023-09-08 19:33:07 +0300

    fix search

--- src/Helpers/Curls/Cbond/CbondCurl.php
+++ src/Helpers/Curls/Cbond/CbondCurl.php
@@ -5,9 +5,7 @@ namespace Common\Helpers\Curls\Cbond;
 use Carbon\Carbon;
 use Common\Helpers\Curls\Curl;
 use Common\Helpers\LoggerHelper;
-use Common\Helpers\Translit;
 use Cache;
-use Common\Models\Catalog\Cbond\CbondStock;
 
 class CbondCurl
 {
@@ -27,31 +25,15 @@ class CbondCurl
                 return Cache::get('cbond' . $searchText);
             }
 
-            [$original, $text, $translitText] = Translit::make($searchText);
             $url = self::API_URL . $searchText;
             $coockies = '';
 
             $response = json_decode(Curl::get($url, [], [], 'cbond', $coockies));
 
-            if (isset($response->result) && $response->result === 'success') {
-                $stock = CbondStock::where(function ($query) use ($searchText) {
-                    $query->where('isin', $searchText)
-                        ->orWhere('shortname', $searchText)
-                        ->orWhere('name', $searchText)
-                        ->orWhere('secid', $searchText);
-                })
-                    ->first();
-
-                if ($stock) {
-                    Cache::add('cbond' . $searchText, $stock, Carbon::now()->addDay());
-                    return $stock;
-                }
-
-                Cache::add('cbond' . $searchText, false, Carbon::now()->addDay());
-                return false;
+            if (isset($response->result) && $response->result === 'success')
+            {
+                Cache::add('cbond' . $searchText, 1, Carbon::now()->addDay());
             }
-
-            return false;
         } catch (Exception $e) {
             LoggerHelper::getLogger('cbond-stock')->error($e);
             return false;
