83a7866a5c98719ad180c91fa811e3962229c7a8
MasyaSm <kuvaldamax@gmail.com>
2023-09-13 15:15:42 +0300

    upd active interface

--- src/Models/Interfaces/Catalog/DefinitionActiveConst.php
+++ src/Models/Interfaces/Catalog/DefinitionActiveConst.php
@@ -2,29 +2,22 @@
 
 namespace Common\Models\Interfaces\Catalog;
 
-use App\Models\Actives\Invests\Quotes\Currency;
-use App\Models\Actives\Invests\Quotes\Pif;
-use App\Models\Actives\Invests\QuotesAndPay\Etf;
-use App\Models\Actives\Invests\QuotesAndPay\Futures;
-use App\Models\Actives\Invests\QuotesAndPay\Obligation;
-use App\Models\Actives\Invests\QuotesAndPay\Stock;
-
 interface DefinitionActiveConst
 {
     /**
      * resources
      */
-    const CB = 1;
-    const YAHOO_QUOTES = 2;
-    const MOSCOW_EXCHANGE_QUOTES = 3;
-    const CUSTOM = 4;
-    const CBONDS = 5;
+    public const CB = 1;
+    public const YAHOO_QUOTES = 2;
+    public const MOSCOW_EXCHANGE_QUOTES = 3;
+    public const CUSTOM = 4;
+    public const CBONDS = 5;
 
     /**
      * periodic
      */
-    const ONCE = 1;
-    const PERIOD = 2;
+    public const ONCE = 1;
+    public const PERIOD = 2;
 
     /**
      * типы в jobs
@@ -45,113 +38,113 @@ interface DefinitionActiveConst
     /**
      * start types
      */
-    const SALARY = 1001;
+    public const SALARY = 1001;
 
-    const CUSTOM_SALARY = 1004;
+    public const CUSTOM_SALARY = 1004;
 
-    const FLAT = 2001;
-    const HOUSE = 2002;
-    const LAND = 2003;
+    public const FLAT = 2001;
+    public const HOUSE = 2002;
+    public const LAND = 2003;
 
-    const CAR = 2101;
-    const MOTO = 2102;
-    const TECHNIC = 2103;
+    public const CAR = 2101;
+    public const MOTO = 2102;
+    public const TECHNIC = 2103;
 
-    const JEWELRY = 2201;
-    const PERSONAL_TECHNIC = 2202;
-    const CUSTOM_PROPERTY = 2203;
+    public const JEWELRY = 2201;
+    public const PERSONAL_TECHNIC = 2202;
+    public const CUSTOM_PROPERTY = 2203;
 
-    const DEPOSIT = 3001;
-    const DEBT = 3002;
+    public const DEPOSIT = 3001;
+    public const DEBT = 3002;
 
-    const COMMODITY = 3101;
-    const CURRENCY = 3102;
-    const CRYPTO = 3103;
-    const ETF = 3104;
-    const PIF = 3105;
-    const HEDGE_FUND = 3106;
-    const BPIF = 3107;
-    const PRECIOUS_METAL = 3108;
+    public const COMMODITY = 3101;
+    public const CURRENCY = 3102;
+    public const CRYPTO = 3103;
+    public const ETF = 3104;
+    public const PIF = 3105;
+    public const HEDGE_FUND = 3106;
+    public const BPIF = 3107;
+    public const PRECIOUS_METAL = 3108;
 
-    const STOCK = 3201;
-    const OBLIGATION = 3202;
-    const BOND = 3202;
-    const STRUCTURE_PRODUCT = 3203;
-    const EXCHANGE_NOTE = 3205;
-    const OBLIGATION_NOTE = 3206;
-    const STRATEGY_DU = 3207;
-    const OPTION = 3208;
-    const FUTURES = 3209;
+    public const STOCK = 3201;
+    public const OBLIGATION = 3202;
+    public const BOND = 3202;
+    public const STRUCTURE_PRODUCT = 3203;
+    public const EXCHANGE_NOTE = 3205;
+    public const OBLIGATION_NOTE = 3206;
+    public const STRATEGY_DU = 3207;
+    public const OPTION = 3208;
+    public const FUTURES = 3209;
 
-    const RENT_CAR = 9001;
-    const RENT_FLAT = 9002;
-    const SPEND_LIFE = 9003;
-    const CUSTOM_OBLIGATION = 9005;
-    const CAR_CREDIT = 9006;
-    const FLAT_CREDIT = 9007;
-    const CREDIT = 9008;
-    const ALIMONY = 9009;
-    const LOAN = 9010;
+    public const RENT_CAR = 9001;
+    public const RENT_FLAT = 9002;
+    public const SPEND_LIFE = 9003;
+    public const CUSTOM_OBLIGATION = 9005;
+    public const CAR_CREDIT = 9006;
+    public const FLAT_CREDIT = 9007;
+    public const CREDIT = 9008;
+    public const ALIMONY = 9009;
+    public const LOAN = 9010;
 
-    const MONEY_ACTIVE = 10001;
+    public const MONEY_ACTIVE = 10001;
 
-    const PRODUCTS_SPEND = 11001;
-    const CAFE_SPEND = 11002;
-    const CAR_SPEND = 11003;
-    const TRANSPORT_SPEND = 11004;
-    const MEDICINE_SPEND = 11005;
-    const BEAUTY_SPEND = 11006;
-    const CLOTHES_ADULT_SPEND = 11007;
-    const CLOTHES_CHILD_SPEND = 11008;
-    const TOYS_SPEND = 11009;
-    const VACATION_SPEND = 11010;
-    const EDUCATION_SPEND = 11011;
-    const RENT_SPEND = 11012;
-    const FUN_SPEND = 11013;
-    const PRESENTS_SPEND = 11014;
-    const SPORT_SPEND = 11015;
-    const TELECOM_SPEND = 11016;
-    const CUSTOM_SPEND = 9004;
+    public const PRODUCTS_SPEND = 11001;
+    public const CAFE_SPEND = 11002;
+    public const CAR_SPEND = 11003;
+    public const TRANSPORT_SPEND = 11004;
+    public const MEDICINE_SPEND = 11005;
+    public const BEAUTY_SPEND = 11006;
+    public const CLOTHES_ADULT_SPEND = 11007;
+    public const CLOTHES_CHILD_SPEND = 11008;
+    public const TOYS_SPEND = 11009;
+    public const VACATION_SPEND = 11010;
+    public const EDUCATION_SPEND = 11011;
+    public const RENT_SPEND = 11012;
+    public const FUN_SPEND = 11013;
+    public const PRESENTS_SPEND = 11014;
+    public const SPORT_SPEND = 11015;
+    public const TELECOM_SPEND = 11016;
+    public const CUSTOM_SPEND = 9004;
 
-    const SALARY_INCOME = 12001;
-    const BONUS_INCOME = 12002;//премия
-    const RETIRE_INCOME = 12003;
-    const RENT_INCOME = 12004;
-    const RELATIVES_INCOME = 12005;
-    const PASSIVE_INCOME = 12006;
-    const AGENT_INCOME = 12007;
-    const CUSTOM_INCOME = 1003;
+    public const SALARY_INCOME = 12001;
+    public const BONUS_INCOME = 12002;//премия
+    public const RETIRE_INCOME = 12003;
+    public const RENT_INCOME = 12004;
+    public const RELATIVES_INCOME = 12005;
+    public const PASSIVE_INCOME = 12006;
+    public const AGENT_INCOME = 12007;
+    public const CUSTOM_INCOME = 1003;
 
     //страховки
-    const FUNDED_LIFE_INSURANCE = 3003;
-    const INVESTMENT_LIFE_INSURANCE = 3204;
-    const AUTO_INSURANCE = 13001;
-    const PROPERTY_INSURANCE = 13002;
-    const HEALTH_INSURANCE = 13003;
-    const VMI_INSURANCE = 13004;
-    const TRAVEL_INSURANCE = 13005;
-    const SPORT_INSURANCE = 13006;
-    const UNIT_LINKED_INSURANCE = 13007;
-    const LOSE_JOB_INSURANCE = 13008;
-    const RESPONSIBILITY_INSURANCE = 13009;
-    const PET_INSURANCE = 13010;
-    const CUSTOM_INSURANCE = 13011;
+    public const FUNDED_LIFE_INSURANCE = 3003;
+    public const INVESTMENT_LIFE_INSURANCE = 3204;
+    public const AUTO_INSURANCE = 13001;
+    public const PROPERTY_INSURANCE = 13002;
+    public const HEALTH_INSURANCE = 13003;
+    public const VMI_INSURANCE = 13004;
+    public const TRAVEL_INSURANCE = 13005;
+    public const SPORT_INSURANCE = 13006;
+    public const UNIT_LINKED_INSURANCE = 13007;
+    public const LOSE_JOB_INSURANCE = 13008;
+    public const RESPONSIBILITY_INSURANCE = 13009;
+    public const PET_INSURANCE = 13010;
+    public const CUSTOM_INSURANCE = 13011;
 
-    const ALPHA_INS = 1001;
-    const INGOS_INS = 1002;
-    const ROSGOS_INS = 1003;
-    const VSK_INS = 1004;
-    const RESO_INS = 1005;
-    const SOGAZ_INS = 1006;
-    const MAKS_INS = 1007;
-    const RENESANS_INS = 1008;
-    const CAPITAL_LIFE_INS = 1009;
-    const SOGLASIE_INS = 1010;
-    const SBER_INS = 1011;
-    const TINKOFF_INS = 1012;
-    const CUSTOM_INS = 2001;
+    public const ALPHA_INS = 1001;
+    public const INGOS_INS = 1002;
+    public const ROSGOS_INS = 1003;
+    public const VSK_INS = 1004;
+    public const RESO_INS = 1005;
+    public const SOGAZ_INS = 1006;
+    public const MAKS_INS = 1007;
+    public const RENESANS_INS = 1008;
+    public const CAPITAL_LIFE_INS = 1009;
+    public const SOGLASIE_INS = 1010;
+    public const SBER_INS = 1011;
+    public const TINKOFF_INS = 1012;
+    public const CUSTOM_INS = 2001;
 
-    const INSURANCE_COMPANY_GROUP = [
+    public const INSURANCE_COMPANY_GROUP = [
         self::ALPHA_INS,
         self::INGOS_INS,
         self::ROSGOS_INS,
@@ -170,7 +163,7 @@ interface DefinitionActiveConst
      * end types
      */
 
-    const INCOME_GROUP = [
+    public const INCOME_GROUP = [
         self::SALARY_INCOME,
         self::BONUS_INCOME,
         self::RETIRE_INCOME,
@@ -180,7 +173,7 @@ interface DefinitionActiveConst
         self::AGENT_INCOME,
     ];
 
-    const INSURANCES_GROUP = [
+    public const INSURANCES_GROUP = [
         self::AUTO_INSURANCE,
         self::PROPERTY_INSURANCE,
         self::HEALTH_INSURANCE,
@@ -195,11 +188,11 @@ interface DefinitionActiveConst
         self::FUNDED_LIFE_INSURANCE
     ];
 
-    const SALARY_GROUP = [
+    public const SALARY_GROUP = [
         self::SALARY
     ];
 
-    const SPENDING_GROUP = [
+    public const SPENDING_GROUP = [
         self::PRODUCTS_SPEND,
         self::CAFE_SPEND,
         self::CAR_SPEND,
@@ -218,7 +211,7 @@ interface DefinitionActiveConst
         self::TELECOM_SPEND,
     ];
 
-    const SPENDING_GROUP_ICON = [
+    public const SPENDING_GROUP_ICON = [
         self::PRODUCTS_SPEND => 'food.svg',
         self::CAFE_SPEND => 'cafe.svg',
         self::CAR_SPEND => 'car.svg',
@@ -237,7 +230,7 @@ interface DefinitionActiveConst
         self::TELECOM_SPEND => 'phone.svg',
     ];
 
-    const INCOME_GROUP_ICON = [
+    public const INCOME_GROUP_ICON = [
         self::SALARY_INCOME => 'salary.svg',
         self::BONUS_INCOME => 'award.svg',
         self::RETIRE_INCOME => 'pension.svg',
@@ -247,13 +240,13 @@ interface DefinitionActiveConst
         self::AGENT_INCOME => 'agent.svg',
     ];
 
-    const SPEND_OBLIGATION_GROUP = [
+    public const SPEND_OBLIGATION_GROUP = [
         self::RENT_CAR,
         self::RENT_FLAT,
         self::SPEND_LIFE
     ];
 
-    const CREDIT_OBLIGATION_GROUP = [
+    public const CREDIT_OBLIGATION_GROUP = [
         self::CAR_CREDIT,
         self::FLAT_CREDIT,
         self::CREDIT,
@@ -261,7 +254,7 @@ interface DefinitionActiveConst
         self::LOAN,
     ];
 
-    const PROPERTY_GROUP = [
+    public const PROPERTY_GROUP = [
         self::FLAT,
         self::HOUSE,
         self::LAND,
@@ -272,7 +265,7 @@ interface DefinitionActiveConst
 //        self::PERSONAL_TECHNIC,
     ];
 
-    const INVEST_GROUP = [
+    public const INVEST_GROUP = [
         self::STOCK,
         self::OBLIGATION,
         self::PIF,
@@ -293,7 +286,7 @@ interface DefinitionActiveConst
         self::DEBT,
     ];
 
-    const PACKAGE_GROUP = [
+    public const PACKAGE_GROUP = [
         self::STOCK,
         self::OBLIGATION,
         self::PIF,
@@ -312,7 +305,7 @@ interface DefinitionActiveConst
         self::HEDGE_FUND,
     ];
 
-    const TICKER_GROUP = [
+    public const TICKER_GROUP = [
         self::CURRENCY,
         self::STOCK,
         self::ETF,
@@ -323,17 +316,17 @@ interface DefinitionActiveConst
     /**
      * period types
      */
-    const DAILY = 1;
-    const WEEKLY = 2;
-    const MONTHLY = 3;
-    const QUARTER = 4;
-    const HALFYEAR = 5;
-    const YEARLY = 6;
-    const CUSTOM_PERIOD_WEEK = 7;
-    const CUSTOM_PERIOD_MONTH = 8;
-    const CONDITION = 9;
+    public const DAILY = 1;
+    public const WEEKLY = 2;
+    public const MONTHLY = 3;
+    public const QUARTER = 4;
+    public const HALFYEAR = 5;
+    public const YEARLY = 6;
+    public const CUSTOM_PERIOD_WEEK = 7;
+    public const CUSTOM_PERIOD_MONTH = 8;
+    public const CONDITION = 9;
 
-    const PERIOD_GROUP = [
+    public const PERIOD_GROUP = [
 //        self::DAILY,
 //        self::WEEKLY,
         self::MONTHLY,
@@ -347,30 +340,30 @@ interface DefinitionActiveConst
     /**
      * action strategy types
      */
-    const PLUS = 1;
-    const MINUS = 2;
-    const EQUAL = 3;
+    public const PLUS = 1;
+    public const MINUS = 2;
+    public const EQUAL = 3;
 
     /**
      *
      */
-    const MONEY = 1;
-    const INVEST = 2;
-    const OWN = 3;
+    public const MONEY = 1;
+    public const INVEST = 2;
+    public const OWN = 3;
 
     /**
      * action_id
      */
-    const BUY = 1;
-    const GET = 2;
+    public const BUY = 1;
+    public const GET = 2;
 
     /**
      * percent debt types
      */
-    const SIMPLE = 1;
-    const DIFFERENTIAL = 2;
+    public const SIMPLE = 1;
+    public const DIFFERENTIAL = 2;
 
-    const DEPOSIT_PERCENT_GROUP = [
+    public const DEPOSIT_PERCENT_GROUP = [
         self::SIMPLE,
         self::DIFFERENTIAL
     ];
@@ -378,28 +371,19 @@ interface DefinitionActiveConst
     /**
      * start group types
      */
-    const STOCK_GROUP_TYPE = 1001;
-    const OBLIGATION_GROUP_TYPE = 1002;
-    const METAL_GROUP_TYPE = 1003;
-    const PROPERTY_GROUP_TYPE = 1004;
-    const ALTERNATIVE_GROUP_TYPE = 1005;
-    const DIRECT_GROUP_TYPE = 1006;
-    const INSTRUMENT_CASH_FLOW_GROUP_TYPE = 1007;
-    const CUSTOM_GROUP_TYPE = 2001;
-
-    public const CATALOG_CLASSES = [
-        'stock' => Stock::class,
-        'obligation' => Obligation::class,
-        'currency' => Currency::class,
-        'etf' => Etf::class,
-        'pif' => Pif::class,
-        'futures' => Futures::class,
-    ];
+    public const STOCK_GROUP_TYPE = 1001;
+    public const OBLIGATION_GROUP_TYPE = 1002;
+    public const METAL_GROUP_TYPE = 1003;
+    public const PROPERTY_GROUP_TYPE = 1004;
+    public const ALTERNATIVE_GROUP_TYPE = 1005;
+    public const DIRECT_GROUP_TYPE = 1006;
+    public const INSTRUMENT_CASH_FLOW_GROUP_TYPE = 1007;
+    public const CUSTOM_GROUP_TYPE = 2001;
 
     /**
      * end group types
      */
-    const GROUP_TYPES_GROUP = [
+    public const GROUP_TYPES_GROUP = [
         self::STOCK_GROUP_TYPE,
         self::OBLIGATION_GROUP_TYPE,
         self::METAL_GROUP_TYPE,
@@ -409,7 +393,7 @@ interface DefinitionActiveConst
         self::INSTRUMENT_CASH_FLOW_GROUP_TYPE
     ];
 
-    const GROUP_TYPES_GROUP_ICON = [
+    public const GROUP_TYPES_GROUP_ICON = [
         self::STOCK_GROUP_TYPE => 'investment.svg',
         self::OBLIGATION_GROUP_TYPE => 'bond.svg',
         self::METAL_GROUP_TYPE => 'metal.svg',
@@ -422,9 +406,9 @@ interface DefinitionActiveConst
     /**
      * class types
      */
-    const CUSTOM_CLASS_TYPE = 2001;
+    public const CUSTOM_CLASS_TYPE = 2001;
 
-    const GROUP_CLASS_TYPES = [
+    public const GROUP_CLASS_TYPES = [
         self::CUSTOM_CLASS_TYPE
     ];
 
