f95580097ad8aeab255099e1635b43f6942d917c
MasyaSm <kuvaldamax@gmail.com>
2023-10-17 14:11:23 +0300

    add new type notification

--- src/Models/Users/UserNotification.php
+++ src/Models/Users/UserNotification.php
@@ -38,6 +38,7 @@ class UserNotification extends BaseModel implements CommonRemoveActiveInterface
     public const CLOSE_INSURANCES = 1002;
     public const TOKEN_IS_NOT_VALID = 1003;
     public const NEGATIVE_TRADES = 1004;
+    public const NOT_FOUND_STOCK = 1005;
 
     public const CONTACT_BIRTHDAY = 2003;
 
