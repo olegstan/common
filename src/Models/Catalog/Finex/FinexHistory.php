1f38526b2b49e3438173e6a81b0910825bed805c
olegstan <olegstan@inbox.ru>
2023-12-06 13:51:36 +0300

    finex history

---
+++ src/Models/Catalog/Finex/FinexHistory.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace Common\Models\Catalog\Finex;
+
+use Cache;
+use Carbon\Carbon;
+use Common\Models\Catalog\BaseCatalog;
+use Common\Models\Catalog\MoscowExchange\MoscowExchangeStock;
+use Common\Models\Interfaces\Catalog\CommonsFuncCatalogHistoryInterface;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+/**
+ * @property $cbond_stock_id
+ * @property Carbon $tradedate
+ * @property $numtrades
+ * @property $value
+ * @property $open
+ * @property $low
+ * @property $high
+ * @property $close
+ * @property $volume
+ * @property $warprice
+ * @property $marketprice2
+ * @property $marketprice3
+ * @property $admitquote
+ * @property $mp2vatrd
+ * @property $marketprice3tradesvalue
+ * @property $waval
+ * @property $faceunit
+ */
+class FinexHistory extends BaseCatalog implements CommonsFuncCatalogHistoryInterface
+{
+    /**
+     * @var string
+     */
+    public $table = 'finex_history';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'cbond_stock_id',
+        'tradedate',
+        'numtrades',
+        'value',
+        'open',
+        'low',
+        'high',
+        'close',
+        'volume',
+        'warprice',
+        'marketprice2',
+        'marketprice3',
+        'admitquote',
+        'mp2vatrd',
+        'marketprice3tradesvalue',
+        'waval',
+        'faceunit',
+    ];
+    /**
+     * @var array
+     */
+    protected $casts = [
+        'cbond_stock_id' => 'integer',
+        'tradedate' => 'datetime',
+        'numtrades' => 'float',
+        'value' => 'float',
+        'open' => 'float',
+        'low' => 'float',
+        'high' => 'float',
+        'close' => 'float',
+        'volume' => 'float',
+        'warprice' => 'float',
+        'marketprice2' => 'float',
+        'marketprice3' => 'float',
+        'admitquote' => 'float',
+        'mp2vatrd' => 'float',
+        'marketprice3tradesvalue' => 'float',
+        'waval' => 'float',
+        'faceunit' => 'string',
+    ];
+
+    public $timestamps = false;
+
+    /**
+     * @return float
+     */
+    public function getValue(): float
+    {
+        return $this->close;
+    }
+
+    /**
+     * @return HasOne
+     */
+    public function item(): HasOne
+    {
+        return $this->hasOne(MoscowExchangeStock::class, 'id', 'cbond_stock_id');
+    }
+
+    /**
+     * @param $query
+     * @param CbondStock $item
+     * @return void
+     */
+    public function scopeByItem($query, CbondStock $item)
+    {
+        $query->where('isin', $item->isin);
+    }
+
+    /**
+     * @param $priceKey
+     * @param $dateKey
+     * @return array
+     */
+    public function setPrice($priceKey, $dateKey)
+    {
+
+    }
+}
