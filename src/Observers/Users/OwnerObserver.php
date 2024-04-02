548a249dfdd2945b7cb599d14a67e2474864fbfe
MasyaSm <kuvaldamax@gmail.com>
2024-04-02 16:16:51 +0300

    add owner observer

---
+++ src/Observers/Users/OwnerObserver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Common\Observers\Users;
+
+use Common\Models\Users\Roles\Types\Owner;
+
+class OwnerObserver
+{
+    /**
+     * @param Owner $user
+     *
+     * @return void
+     */
+    public function creating(Owner $user)
+    {
+
+    }
+
+    /**
+     * @param Owner $user
+     *
+     * @return void
+     */
+    public function created(Owner $user)
+    {
+        $user->createAccounts();
+    }
+
+    /**
+     * @param Owner $user
+     *
+     * @return void
+     */
+    public function updated(Owner $user)
+    {
+    }
+
+    /**
+     * @param Owner $user
+     *
+     * @return void
+     */
+    public function deleted(Owner $user)
+    {
+
+    }
+
+    /**
+     * @param Owner $user
+     *
+     * @return void
+     */
+    public function deleting(Owner $user)
+    {
+    }
+}
