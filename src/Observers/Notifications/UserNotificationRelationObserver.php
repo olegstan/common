0875c93952b9908edae45fcb969b86855095d446
MasyaSm <kuvaldamax@gmail.com>
2024-05-07 13:18:25 +0300

    upd observe notif

--- src/Observers/Notifications/UserNotificationRelationObserver.php
+++ src/Observers/Notifications/UserNotificationRelationObserver.php
@@ -3,6 +3,7 @@
 namespace Common\Observers\Notifications;
 
 use Common\Models\Users\Notification\UserNotification;
+use Common\Models\Users\Notification\UserNotificationRelation;
 
 class UserNotificationRelationObserver
 {
@@ -33,6 +34,34 @@ class UserNotificationRelationObserver
      */
     public function updated(UserNotification $model)
     {
+        //TODO оптимизировать, чтобы это проверялось только один раз, для одного уведомления за запрос, в конце
+        $parent = UserNotification::where('id', $model->notification_id)
+            ->with('items')
+            ->first();
+
+        if($parent)
+        {
+            //считаем все связанные итемы в уведомлениии
+            //если 0 неподтвержденных, значит всё уведомление может поменять статус
+            $countNotConfirmed = 0;
+
+            $parent->items()->each(function ($item) use (&$countNotConfirmed){
+                /**
+                 * @var UserNotificationRelation $item
+                 */
+                if(!$item->is_confirmed)
+                {
+                    $countNotConfirmed++;
+                }
+            });
+
+            if($countNotConfirmed === 0)
+            {
+                $parent->update([
+                    'status' => UserNotification::CONFIRMED
+                ]);
+            }
+        }
     }
 
     /**
