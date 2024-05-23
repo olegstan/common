784d5937cbf75f424960ebc698ad94463a313869
MasyaSm <kuvaldamax@gmail.com>
2024-05-23 15:34:33 +0300

    add log file queue

--- src/Jobs/Base/BaseRabbitMQJob.php
+++ src/Jobs/Base/BaseRabbitMQJob.php
@@ -2,7 +2,6 @@
 
 namespace Common\Jobs\Base;
 
-use App\Helpers\BrokerParsers\Aton\AtonParser;
 use Common\Helpers\LoggerHelper;
 use Common\Helpers\Queue\RabbitMQQueue;
 use Exception;
@@ -14,34 +13,45 @@ use PhpAmqpLib\Message\AMQPMessage;
 
 class BaseRabbitMQJob extends RabbitMQJob
 {
-    public function __construct(Container $container,
-        RabbitMQQueue $rabbitmq,
-        AMQPMessage $message,
-        string $connectionName,
-        string $queue)
+    /**
+     * Конструктор класса BaseRabbitMQJob.
+     *
+     * @param Container $container Экземпляр контейнера.
+     * @param RabbitMQQueue $rabbitmq Экземпляр RabbitMQQueue.
+     * @param AMQPMessage $message Экземпляр AMQPMessage.
+     * @param string $connectionName Имя соединения.
+     * @param string $queue Имя очереди.
+     */
+    public function __construct($container, $rabbitmq, $message, $connectionName, $queue)
     {
+        parent::__construct($container, $rabbitmq, $message, $connectionName, $queue);
+
+        // Проверяем, включено ли расширенное ведение журнала
         if (config('app.extended_log')) {
+            // Устанавливаем командную клавишу для ведения журнала
             LoggerHelper::$commandKey = 'queue';
 
-            $payload = $message->payload;
+            $payload = $message->getBody();
 
+            // Проверяем, не пуста ли полезная нагрузка
             if ($payload) {
-                $jsonDecoded = json_decode($message->payload, true);
+                $jsonDecoded = json_decode($message->getBody(), true);
 
+                // Проверяем, установлен ли ключ 'job' в декодированном JSON
                 if (isset($jsonDecoded['job'])) {
                     $path = explode('\\', $jsonDecoded['job']);
                     $count = count($path);
 
+                    // Проверяем, существует ли последний элемент пути
                     if (isset($path[$count - 1])) {
+                        // Устанавливаем ключ задания для регистрации
                         LoggerHelper::$jobKey = strtolower($path[$count - 1]);
                     }
                 }
             }
 
+            // Прослушиваем запросы к базе данных и записываем их при необходимости
             DB::listen(function ($sql) {
-                /**
-                 *
-                 */
                 $key = $sql->time > 100 ? 'slow-query' : 'query';
                 $sqlWithBindings = $sql->sql;
 
@@ -53,18 +63,11 @@ class BaseRabbitMQJob extends RabbitMQJob
 
                     LoggerHelper::getLogger($key)->debug(
                         'SQL => ' . $sqlWithBindings . PHP_EOL .
-                        'TIME => ' . $sql->time . ' milliseconds' . PHP_EOL
+                        'TIME => ' . $sql->time . ' milliseconds' . PHP_EOL,
                     );
                 }
             });
         }
-
-        $this->container = $container;
-        $this->rabbitmq = $rabbitmq;
-        $this->message = $message;
-        $this->connectionName = $connectionName;
-        $this->queue = $queue;
-        $this->decoded = $this->payload();
     }
 
     /**
