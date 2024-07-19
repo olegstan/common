18c20fbcd8be278854fd207a74fc12c6b76fe7a3
olegstan <olegstan@inbox.ru>
2024-07-19 22:47:57 +0300

    fix

--- src/Models/Traits/DuplicateTrait.php
+++ src/Models/Traits/DuplicateTrait.php
@@ -2,6 +2,7 @@
 namespace Common\Models\Traits;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * Trait DuplicateTrait
@@ -48,5 +49,53 @@ trait DuplicateTrait
         $newRelation = $this->replicate();
 
         return $to->{$relationName}()->create($newRelation->toArray());
+    }
+
+    /**
+     * @param array $relations
+     * @return \Common\Models\BaseModel
+     */
+    public function replicateWithRelationsForProcessing(array $relations)
+    {
+        // Создаем копию текущей модели
+        $newModel = $this->replicate();
+
+        // Парсим связи и копируем их
+        foreach ($relations as $relation) {
+            $this->replicateRelation($newModel, $relation);
+        }
+
+        return $newModel;
+    }
+
+    /**
+     * @param Model $newModel
+     * @param $relation
+     */
+    protected function replicateRelation(Model $newModel, $relation)
+    {
+        $relationParts = explode('.', $relation);
+        $mainRelation = array_shift($relationParts);
+        $nestedRelations = implode('.', $relationParts);
+
+        $relatedItems = $this->$mainRelation;
+
+        if ($relatedItems instanceof Collection) {
+            $newCollection = new Collection();
+            foreach ($relatedItems as $relatedItem) {
+                $newRelatedItem = $relatedItem->replicate();
+                if ($nestedRelations) {
+                    $relatedItem->replicateRelation($newRelatedItem, $nestedRelations);
+                }
+                $newCollection->push($newRelatedItem);
+            }
+            $newModel->setRelation($mainRelation, $newCollection);
+        } elseif ($relatedItems instanceof Model) {
+            $newRelatedItem = $relatedItems->replicate();
+            if ($nestedRelations) {
+                $relatedItems->replicateRelation($newRelatedItem, $nestedRelations);
+            }
+            $newModel->setRelation($mainRelation, $newRelatedItem);
+        }
     }
 }
