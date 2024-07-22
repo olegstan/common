91d4c9ad4020f34e94adb3aede24ea5bbb2e2315
olegstan <olegstan@inbox.ru>
2024-07-22 16:16:58 +0300

    fix

--- src/Helpers/Queue/RabbitMQQueue.php
+++ src/Helpers/Queue/RabbitMQQueue.php
@@ -64,7 +64,7 @@ class RabbitMQQueue extends BaseQueue
             return true;
         }
 
-        if (Cache::has($data['cache_key'])) {
+        if (Cache::tags(['job'])->has($data['cache_key'])) {
             $this->logError(__FUNCTION__, 'Такой ключ уже существует', $data);
             return true;
         }
