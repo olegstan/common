1100dcc2d42e053566dbbdf5ac7931e1bad82f14
Masya Smerdov <61746307+MasyaSmv@users.noreply.github.com>
2024-08-13 17:59:32 +0300

    Update User.php

--- src/Models/Users/User.php
+++ src/Models/Users/User.php
@@ -125,6 +125,7 @@ class User extends BaseModel implements AuthenticatableContract, CanResetPasswor
     public static $documentPath = '/storage/document/';
 
     private array $aton_configs = [];
+    private array $client_ids = [];
 
     public const DEFAULT_RETIRE_AGE = 60;
     public const DEFAULT_DEAD_AGE = 25;
@@ -434,51 +435,48 @@ class User extends BaseModel implements AuthenticatableContract, CanResetPasswor
     }
 
     /**
+     * Возвращает все айдишники клиентов
+     *
      * @return array
      */
-    public function getClientIds()
+    public function getClientIds(): array
     {
-        switch ($this->getRole()) {
-            case User::MANAGER:
-                return Client::where(function ($query) {
-                    $query->where('manager_id', $this->id);
-                })
-                    ->pluck('id')
-                    ->toArray();
-            case User::OWNER:
-                $managerIds = UserCollectiveGroup::where('user_id', $this->id)
-                    ->pluck('union_user_id')
-                    ->toArray();
+        $clientIds = $this->aton_configs;
 
-                return Client::where(function ($query) use ($managerIds) {
-                    $query->where('manager_id', $this->id)
-                        ->orWhereIn('manager_id', $managerIds);
-                })
-                    ->pluck('id')
-                    ->toArray();
-                break;
-            case User::DIRECTOR:
-
-                break;
-            case User::ASSISTANT:
-
-                break;
-            case User::ACCOUNTANT:
-
-                break;
-            case User::PARTNER:
-
-                break;
-            case User::DRIVER:
-
-                break;
-            case User::CLIENT:
-
-                break;
+        if (empty($clientIds)) {
+            $this->setAtonConfigs();
         }
 
-        return [];
+        return $clientIds;
     }
+
+    /**
+     * Записывает айдишники клиентов
+     *
+     * @return $this
+     */
+    public function setClientIds(): User
+    {
+        $managerIds = [$this->id];
+
+        if ($this->getRole() === User::OWNER) {
+            $managerIds = array_merge(
+                $managerIds,
+                UserCollectiveGroup::where('user_id', $this->id)
+                    ->pluck('union_user_id')
+                    ->toArray()
+            );
+        }
+
+        if (in_array($this->getRole(), [User::MANAGER, User::OWNER])) {
+            $this->client_ids = Client::whereIn('manager_id', $managerIds)
+                ->pluck('id')
+                ->toArray();
+        }
+
+        return $this;
+    }
+
 
     /**
      * @return array
