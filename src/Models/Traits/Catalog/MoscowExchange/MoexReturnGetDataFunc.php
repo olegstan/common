fa3e7b5f924c51b5d5f6b801082ef5346238e4c1
olegstan <olegstan@inbox.ru>
2024-10-02 11:52:27 +0300

    fix

--- src/Models/Traits/Catalog/MoscowExchange/MoexReturnGetDataFunc.php
+++ src/Models/Traits/Catalog/MoscowExchange/MoexReturnGetDataFunc.php
@@ -229,11 +229,12 @@ trait MoexReturnGetDataFunc
 
         // Проверка сплитов до заданной даты
         if ($date) {
-            $cacheKey = "moex_last_split_{$this->id}_$date";
+            $dateFormatted = $date->format('Y-m-d');
+            $cacheKey = "moex_last_split_{$this->id}_$dateFormatted";
 
-            $split = Cache::remember($cacheKey, 60 * 60, function () use ($date) {
+            $split = Cache::remember($cacheKey, 60 * 60, function () use ($dateFormatted) {
                 return MoscowExchangeSplit::where('moex_stock_id', $this->id)
-                    ->where('date', '<=', $date)
+                    ->whereDate('date', '<=', $dateFormatted)
                     ->orderByDesc('date')
                     ->first();
             });
