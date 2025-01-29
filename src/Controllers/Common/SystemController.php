e75034af309244aec2fc73cb2377cf39ca252420
MasyaSm <kuvalda-max@mail.ru>
2025-01-29 17:13:46 +0300

    add system controller

---
+++ src/Controllers/Common/SystemController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Api\V1\Controllers\Common;
+
+use App\Api\V1\Controllers\Common\BaseController;
+use App\Helpers\FileHelper;
+use Illuminate\Http\Response;
+
+class SystemController extends BaseController
+{
+    /**
+     * @return Response
+     */
+    public function getSystemSettings()
+    {
+        // Получение максимального размера из ini файла
+        $maxFileSize = FileHelper::getMaxUploadSize();
+        return $this->response()->json([
+            'max_post_size' => $maxFileSize,
+        ]);
+    }
+}
