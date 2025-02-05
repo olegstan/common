0906746a0953543ebd0e4d347327bfe4a924e61e
olegstan <olegstan@inbox.ru>
2025-02-05 21:20:27 +0300

    logger for loki

--- src/Helpers/LokiLogger.php
+++ src/Helpers/LokiLogger.php
@@ -2,22 +2,36 @@
 namespace Common\Helpers;
 
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Storage;
+use Exception;
 
 class LokiLogger
 {
+    /**
+     * @var array
+     */
     private static array $buffer = [];
+    /**
+     * @var int
+     */
     private static int $batchSize = 100;
 
     /**
-     * @param string $message
+     * @param $message
+     * @param $app
      * @param string $level
      */
-    public static function log(string $message, string $level = 'info')
+    public static function log($message, $app = 'laravel', string $level = 'info')
     {
+        $formattedMessage = str_replace(PHP_EOL, "\n", $message);
+
+        $timestamp = (int) floor(microtime(true) * 1e9);
+
+// Превращаем в строку
+        $timestampString = (string) $timestamp;
+
         self::$buffer[] = [
-            'stream' => ['app' => 'laravel', 'level' => $level],
-            'values' => [[(string) (microtime(true) * 1e9), $message]]
+            "stream" => ["app" => $app, "level" => $level],
+            "values" => [[$timestampString, json_encode($formattedMessage, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES)]]
         ];
 
         if (count(self::$buffer) >= self::$batchSize) {
@@ -33,11 +47,18 @@ class LokiLogger
         if (empty(self::$buffer)) return;
 
         try {
-            Http::post('http://localhost:3100/loki/api/v1/push', [
+            $response = Http::post('http://localhost:3100/loki/api/v1/push', [
                 'streams' => self::$buffer
             ]);
+
+            if($response->status() !== 200)
+            {
+                throw new Exception('Ошибка отправки в loki, статус ответа ' . $response->status());
+            }
+
             self::$buffer = []; // Очищаем буфер при успехе
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
+            LoggerHelper::getLogger('loki')->error($e->getMessage());
             //TODO написть отправку уведомлений если вдруг loki не доступен
             // Если отправка не удалась, сохраняем в файл
             self::$buffer = [];
